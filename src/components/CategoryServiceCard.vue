8a3b20fa623d3cb2f87ccd4987578a97508630ab
IrfSuar <iasl96@msn.com>
2019-10-30 18:11:18 -0600

    All the components

---
+++ src/components/CategoryServiceCard.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="card text-center">
+        <img src="../assets/img/anthony-reungere-6bObR3bngCA-unsplash.jpg" class="card-img-top" alt="Placeholder">
+        <div class="card-body">
+            <h5 class="card-title list-services border-0">Sillas Don Peter</h5>
+            <p class="card-text">Las mejores sillas del condado.Las mejores sillas del condado.Las mejores sillas del condado.Las mejores sillas del condado.Las mejores sillas del condado.</p>
+            <a href="#" class="btn btn-primary btn-evntu">Generar órden de compra</a>
+            <div class="service-price-container">
+                <h3 class="service-price">$20mxn</h3>
+                <h4 class="service-measurement"> p/hora</h4>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryServiceCard'
+}
+</script>
+
+<style>
+.list-services-content .card {
+    border-radius: 50px 12px 5px!important;
+    border: none;
+    margin-bottom: 20px;
+}
+
+.list-services-content .card-text {
+    color: #333333;
+    font-weight: normal!important;
+    font-size: .8em;
+}
+
+.list-services-content .card:hover {
+    border: solid #ffff00 3px;
+    box-shadow: 0px 0px 25px #0000009e;
+    transition: ease-in 0.3s;
+}
+
+.list-services {
+    font-size: 2em;
+    color: black;
+}
+
+.btn-evntu {
+    background-color: #ffff00!important;
+    color: #000000;
+    font-size: 1em;
+    border-radius: 12px;
+    border: none;
+    box-shadow: 2px 2px 15px rgba(0, 0, 0, 0.15);
+    padding: 9px 25%;
+}
+
+.btn-evntu:hover {
+    border: solid 2px;
+    border-color: black!important;
+    background-color: #ffff00!important;
+    color: #000000;
+    transition: .4 ease;
+}
+
+.service-price-container {
+    display: flex;
+    justify-content: center;
+}
+
+.service-price {
+    margin-right: .4em;
+}
+
+.list-services-content img {
+    border-radius: 50px 12px 5px!important;
+    max-width: 100%!important;
+}
+</style>
---
+++ src/components/NewEventForm.vue
@@ -0,0 +1,93 @@
+<template>
+ <div class="container-fluid">
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-card card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Sube tu servicio</h5>
+                                    <p>Recuerda ser lo más específico posible y escribir un buen script de venta para aumentar las posibilidades de ser seleccionado.</p>
+                                    <form>
+                                        <div class="form-group">
+                                            <label for="formGroupInput">Nombre de tu servicio</label>
+                                            <input type="text-area" class="form-control" id="formGroupInput" placeholder="Mesas 'el buena onda'">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputStateService">Categoría de tu servicio</label>
+                                            <select id="inputStateService" class="form-control custom-select">
+                                                        <option selected>Elegir</option>
+                                                        <option>Spot</option>
+                                                        <option>Comida</option>
+                                                        <option>Decoración</option>
+                                                        <option>Lo que necesitas</option>
+                                            </select>
+                                        </div>
+                                        <label>Sube la mejor foto de tu servicio</label>
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input form-control" id="customFile">
+                                            <label class="custom-file-label" for="customFile">Selecciona tu foto</label>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col">
+                                                <label for="validationCustomUsername">Precio Unitario</label>
+                                                <div class="input-group">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text" id="inputGroupPrepend">$</span>
+                                                    </div>
+                                                    <input type="text" class="form-control" id="validationCustomUsername" placeholder="$00.00" aria-describedby="inputGroupPrepend" required>
+                                                    <div class="invalid-feedback">
+                                                        Introduce correctamente el precio por unidad
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <label for="inputState">Unidad de medida</label>
+                                                <select id="inputState" class="form-control custom-select">
+                                                                    <option selected>Elegir</option>
+                                                                    <option>Hora</option>
+                                                                    <option>Persona</option>
+                                                                    <option>Servicio</option>
+                                                                    <option>Lo que necesitas</option>
+                                                        </select>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+</template>
+
+<script>
+export default {
+  name: 'NewEventForm'
+}
+</script>
+
+<style>
+
+.form-card {
+    background-color: #252B48;
+    color: white;
+    border-radius: 8px;
+    box-shadow: 0px 0px 20px #171B3A;
+}
+
+.form-card label {
+    color: #ffff00;
+    text-align:left!important;
+    display:flex;
+}
+
+.form-card:hover {
+    border-radius: 20px;
+    box-shadow: 0px 0px 25px #0000009e;
+    border: none;
+    transition: ease-in .2s;
+}
+
+.form-card h5 {
+    color: #EFF3F9;
+    font-size: 2.5em;
+}
+</style>
