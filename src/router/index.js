af03103ebd53cfdf4fb7e1f16a4a56fc23b7d2f1
IrfSuar <iasl96@msn.com>
2019-11-24 16:04:46 -0600

    this is the last functional version of our last repository from our first KodeAwards

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from '@/views/Login';
+import RegisterAcount from '@/views/RegisterAcount';
+import Dashboard from '@/views/Dashboard';
+import DashboardProviders from '@/views/DashboardProviders';
+import Index from '@/views/Index';
+import Landing from '@/views/Landing';
+import MyEvents from '@/components/MyEvents';
+Vue.use(Router)
+
+export default new Router({
+    routes: [{
+            path: '/',
+            name: 'Landing',
+            component: Landing
+        },
+        {
+            path: '/register',
+            name: 'RegisterAcount',
+            component: RegisterAcount
+        },
+        {
+            path: '/dashboard',
+            name: 'Dashboard',
+            component: Dashboard
+        },
+        {
+            path: '/Login',
+            name: 'Login',
+            component: Login
+        },
+        {
+            path: '/dashboard-providers',
+            name: 'DashboardProviders',
+            component: DashboardProviders
+        },
+        {
+            path: '/MyEvents',
+            name: 'MyEvents',
+            component: MyEvents
+        }
+    ]
+})
